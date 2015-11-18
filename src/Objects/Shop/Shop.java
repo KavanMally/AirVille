eaf53b4589ad8343ae5577d27273c00acd1b6304
Kavan Mally <kavan.mally@gmail.com>
2015-11-18 16:39:44 -0500

    build shop, made singleton

--- src/Objects/Shop/Shop.java
+++ src/Objects/Shop/Shop.java
@@ -4,4 +4,24 @@ package Objects.Shop;
  * Created by kavan on 11/15/15.
  */
 public class Shop {
+
+
+    private static Shop shop = null;
+
+    protected Shop(){}
+    
+    public static Shop getShop(){
+        if(shop == null)
+            shop = new Shop();
+        return shop;
+    }
+
+
+    public void buyDiamonds(){
+        System.out.println("Coming soon!");
+    }
+
+    public void buyWithDiamonds(){
+        System.out.println("Coming soon!");
+    }
 }
